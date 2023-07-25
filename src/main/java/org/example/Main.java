ffd84782e092041ab84b4c092d235156f983bece
HailanElvf <3263718994@qq.com>
2023-07-25 21:21:06 +0800

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
-
+    Master master=new Master();
+    Menu menu=new Menu();
+    User user=new User();
+    LogIn logIn=new LogIn();
+    IsTrueEnter isTrueEnter=new IsTrueEnter();
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
